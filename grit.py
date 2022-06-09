c372a9982cc0d75b07cf2680a91d06c9c59093c4
Nick Jagiella <nick.jagiella@gmail.com>
2022-06-09 16:32:11 +0200

    add grit script and multiple sprites

---
+++ grit.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  9 12:20:03 2022
+
+@author: nickj
+"""
+import sys
+import argparse
+import cv2
+import os
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('imagefile', type=str,
+                    help='an integer for the accumulator')
+args = parser.parse_args(sys.argv[1:])
+
+img = cv2.imread(args.imagefile)
+
+
+# img = cv2.resize(img, (8, 8))
+# cv2.imwrite(args.imagefile, img)
+print(img.shape)
+
+
+def rgb888to333(img):
+    return [[[c >> 5 for c in pix] for pix in row] for row in img]
+
+
+img = rgb888to333(img)
+print(img)
+
+
+def tofile(filename, img):
+    base = os.path.basename(filename)
+    suffix = os.path.splitext(base)[0]
+    print(suffix)
+
+    with open(filename, 'w+') as fp:
+
+        pal = []
+        fp.write('u16 %sPalLen = 16;\n' % (suffix))
+        fp.write('u16 %sPal[16] = {\n' % (suffix))
+        for row in img:
+            for pix in row:
+                if(pix not in pal):
+                    pal.append(pix)
+                    fp.write('\t0x%04X, \n' %
+                             ((pix[0] << 9) + (pix[1] << 5) + (pix[2] << 1)))
+        for _ in range(len(pal), 16):
+            fp.write('\t0x0000,\n')
+        fp.write('};\n')
+        print('palette: '+str(pal))
+
+        fp.write('u32 %sTilesLen = 8;\n' % (suffix))
+        fp.write('u32 %sTiles[] = {\n' % (suffix))
+
+        for col_i in range(0, len(img[0]), 8):
+            for row_i in range(0, len(img), 8):
+                for row in img[row_i:(row_i+8)]:
+                    print('row: ' + str(row))
+
+                    # for i in range(0, len(row), 8):
+
+                    fp.write('\t0x')
+                    for j in range(col_i, col_i+8):
+                        index = pal.index(row[j])
+                        fp.write('%X' % (index))
+                    fp.write(', \n')
+        fp.write('};\n')
+
+
+tofile(os.path.splitext(args.imagefile)[0] + '.h', img)
--- smd_test.c
+++ smd_test.c
@@ -1,9 +1,37 @@
 #include "genesis.h"
+#include "kirby.h"
+#include "mushroom.h"
 
 
 int main(bool hardReset)
 {
-    VDP_drawText("Hello world !", 12, 12);
+	int kirbyTid = TILE_USERINDEX+0;
+	int kirbyPid = PAL1;
+	int mushroomTid = TILE_USERINDEX+1;
+	int mushroomPid = PAL2;
+
+	VDP_setPalette(/*pid*/kirbyPid, kirbyPal);
+	VDP_setPalette(/*pid*/mushroomPid, mushroomPal);
+
+	VDP_loadTileData(kirbyTiles, kirbyTid, 4, CPU);
+	VDP_loadTileData(mushroomTiles, mushroomTid, 4, CPU);
+
+	VDPSprite sprites[2];
+
+	sprites[0].x = 0x80 + 10;
+	sprites[0].y = 0x80 + 10;
+	sprites[0].size = SPRITE_SIZE(1,1);
+	sprites[0].link = 1;
+	sprites[0].attribut = TILE_ATTR_FULL(kirbyPid, 1, 0, 0, kirbyTid);
+
+	sprites[1].x = 0x80 + 50;
+	sprites[1].y = 0x80 + 50;
+	sprites[1].size = SPRITE_SIZE(2,2);
+	sprites[1].link = 0;
+	sprites[1].attribut = TILE_ATTR_FULL(mushroomPid, 1, 0, 0, mushroomTid);
+	DMA_transfer(CPU, DMA_VRAM, sprites, VDP_SPRITE_TABLE, (sizeof(VDPSprite) * 2) / 2, 2);
+
+    //VDP_drawText("Hello world !", 12, 12);
 
     while(TRUE)
     {
